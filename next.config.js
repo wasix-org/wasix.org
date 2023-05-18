00d37c7344655228fd0f9ede93b1477f48f5080f
Rudra Arora <aroraru@tcd.ie>
2023-05-18 17:38:50 +0100

    added deployment config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
 
-module.exports = withNextra()
+module.exports = {
+  ...withNextra(),
+  images: {
+    unoptimized: true,
+  },
+};
